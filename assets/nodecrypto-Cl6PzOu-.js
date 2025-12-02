a5ff1c0f52ce814f67ae6845620ce0526eee40cc
lucio0256 <lucio0256>
2025-12-03 00:22:26 +0100

    Updates

---
+++ assets/nodecrypto-Cl6PzOu-.js
@@ -0,0 +1,1 @@
+import{c}from"./utils-C9aF12dU.js";import"./index-amI80QpE.js";const y=async n=>{const{content:t,publicKey:r,privateKey:i}=n,o=(void 0)({key:c({uncompressedPrivateKeyHex:i,compressedPublicKeyHex:r}),format:"jwk"}),e=(void 0)("SHA256");return e.write(Buffer.from(t)),e.end(),e.sign(o,"hex")};export{y as signWithApiKey};
